fd43614e473527d9c21adc0fb74c34c6a0b09944
henqchav <henqchav@espol.edu.ec>
2023-06-16 00:42:19 -0500

    dashboard front

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,67 +1,13 @@
 'use client';
-import { Sidebar } from 'flowbite-react';
-import { useState } from 'react';
-import { RiPlantFill,RiHome2Fill } from 'react-icons/ri';
-import { GiPlantsAndAnimals } from 'react-icons/gi'
-import { FaUserAlt } from 'react-icons/fa'
+import SidebarC from '@/app/components/sidebar/SidebarC';
 
-const Page = () => {
-  const [selectedOption, setSelectedOption] = useState('');
-
-  const handleOptionClick = (option: string) => {
-    setSelectedOption(option);
-  };
-
+const DashboardIndexPage = () => {
   return (
-    <Sidebar 
-      style={{ height: '100vh'  }}
-      className="bg-red-500 hover:bg-red-600"
-    >
-      <Sidebar.Logo
-        href="#"
-        img="public\images\logo-w.svg"
-        imgAlt="logo"
-      >
-        <p>
-          Arborista
-        </p>
-      </Sidebar.Logo>
-      <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Sidebar.Collapse
-            label="Usuarios"
-            icon={FaUserAlt}
-          >
-            <Sidebar.Item href="#">Agregar Usuarios</Sidebar.Item>
-            <Sidebar.Item href="#">Modificar/Eliminar Usuarios</Sidebar.Item>
-          </Sidebar.Collapse>
-
-          <Sidebar.Collapse 
-            label="Especies"
-            icon={RiPlantFill}
-          >
-            <Sidebar.Item href="#">Agregar Especies</Sidebar.Item>
-            <Sidebar.Item href="#">Modificar/Eliminar Especies</Sidebar.Item>
-          </Sidebar.Collapse>
-
-          <Sidebar.Collapse 
-            label="Viveros"
-            icon={GiPlantsAndAnimals}
-          >
-            <Sidebar.Item href="#">Agregar Viveros</Sidebar.Item>
-            <Sidebar.Item href="#">Modificar/Eliminar Viveros</Sidebar.Item>
-          </Sidebar.Collapse>
-
-          <Sidebar.Item 
-            href="#" 
-            icon={RiHome2Fill}
-          >
-            <p>Regreso al Inicio</p>
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
-    </Sidebar>
+    <div>
+      <SidebarC />
+      {/* Contenido de la página de inicio del dashboard */}
+    </div>
   );
 };
 
-export default Page;
+export default DashboardIndexPage;
---
+++ app/dashboard/usuario/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+import SidebarC from '@/app/components/sidebar/SidebarC';
+import { Tabs } from 'flowbite-react';
+
+const DashboardIndexPage = () => {
+  return (
+    <div style={{ display: 'flex' }}>
+      <SidebarC />
+      <div style={{ flex: 1, marginLeft: '20px', marginTop:'100px', marginRight:'50px' }}>
+        <Tabs.Group
+        aria-label="Tabs with icons"
+        style='underline'
+      >
+        <Tabs.Item
+          active
+          title="Registrar Usuario"
+        >
+          <p>
+            This is
+            <span className="font-medium text-gray-800 dark:text-white">
+              Profile tab's associated content
+            </span>
+            .
+            Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+            control the content visibility and styling.
+          </p>
+        </Tabs.Item>
+        
+      </Tabs.Group>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardIndexPage;
