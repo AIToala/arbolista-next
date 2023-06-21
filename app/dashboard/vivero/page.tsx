6dd089f7dc7d44c61c9d94adbae57c23499db3fb
henqchav <henqchav@espol.edu.ec>
2023-06-21 00:35:02 -0500

    dashboard/viveros changes

--- app/dashboard/vivero/page.tsx
+++ app/dashboard/vivero/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import SidebarC from '@/app/components/sidebar/SidebarC';
-import { Tabs } from 'flowbite-react';
+import { Tabs, Label, TextInput, FileInput } from 'flowbite-react';
 
 const DashboardIndexPage = () => {
   return (
@@ -15,15 +15,93 @@ const DashboardIndexPage = () => {
           active
           title="Registrar Vivero"
         >
-          <p>
-            This is
-            <span className="font-medium text-gray-800 dark:text-white">
-              Profile tabs associated content
-            </span>
-            .
-            Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-            control the content visibility and styling.
-          </p>
+          <div className="flex max-w-md flex-col gap-4">
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="name"
+                  value="Nombre"
+                />
+              </div>
+              <TextInput
+                id="name"
+                sizing="md"
+                type="text"
+                placeholder='Ingrese el nombre del vivero'
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="address"
+                  value="Direccion"
+                />
+              </div>
+              <TextInput
+                id="address"
+                sizing="md"
+                type="text"
+                placeholder='Ingrese la direccion del vivero'
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="phone"
+                  value="Número de telefono"
+                />
+              </div>
+              <TextInput
+                id="phone"
+                sizing="md"
+                type="text"
+                placeholder='Ingrese el numero de telefono del vivero'
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="email"
+                  value="Correo electronico"
+                />
+              </div>
+              <TextInput
+                id="email"
+                sizing="md"
+                type="text"
+                placeholder='Ingrese el correo electronico del vivero'
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="website"
+                  value="Sitio web"
+                />
+              </div>
+              <TextInput
+                id="website"
+                sizing="md"
+                type="text"
+                placeholder='Ingrese el url del sitio web del vivero'
+              />
+            </div>
+            <div
+              className="max-w-md"
+              id="fileUpload"
+              >
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="logo-src"
+                  value="Upload file"
+                />
+              </div>
+              <FileInput
+                helperText="Selecciona el logo del vivero"
+                id="logo-src"
+              />
+              </div>
+          </div>
         </Tabs.Item>
       </Tabs.Group>
       </div>
