62990cad8dcac64846a598a4cf80b355923eabec
Andres Toala <andrestoala2013@gmail.com>
2023-06-20 20:02:07 -0500

    Modularizacion de opciones de selecciones

--- app/components/buttons/FilterButton.tsx
+++ app/components/buttons/FilterButton.tsx
@@ -3,20 +3,8 @@ import { useState } from "react";
 import Select from "react-select";
 import { Dropdown } from "flowbite-react";
 import {
-  ConservationStatus,
-  RootingTypes,
-  FlowerArrangement,
-  FloweringSeason,
-  LeafPersistence,
-  StemLeafPosition,
-  LeafComposition,
-  FruitType,
-  Origin,
-  LightRequirement,
-  GrowthRate,
-  PriorityLevel,
-  CrownWidth,
-} from "@prisma/client";
+  speciesEnums,
+} from "@/app/types/index";
 import { DateRange } from "react-date-range";
 
 const FilterButton = () => {
@@ -26,157 +14,7 @@ const FilterButton = () => {
   const limpiar = () => {
     console.log("limpiar");
   };
-  const conservationStatus = Object.values(ConservationStatus).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const rootingTypes = Object.values(RootingTypes).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const flowerArrangement = Object.values(FlowerArrangement).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const floweringSeason = Object.values(FloweringSeason).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const leafPersistence = Object.values(LeafPersistence).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const stemLeafPosition = Object.values(StemLeafPosition).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const leafComposition = Object.values(LeafComposition).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const fruitType = Object.values(FruitType).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const origin = Object.values(Origin).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const lightRequirement = Object.values(LightRequirement).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const growthRate = Object.values(GrowthRate).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const priorityLevel = Object.values(PriorityLevel).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-  const crownWidth = Object.values(CrownWidth).map((value) => ({
-    value,
-    label: value.replace("_"," "),
-  }));
-
-  const altitudeRange = [
-    { value: "No determinado", label: "No determinado" },
-    { value: "0-1000 msnm", label: "0-1000" },
-    { value: "1001-1500 msnm", label: "1000-2000" },
-    { value: "1501-2000 msnm", label: "1501-2000" },
-    { value: "2001-2500 msnm", label: "2001-2500" },
-    { value: "2501-3000 msnm", label: "2501-3000" },
-    { value: "3001-3500 msnm", label: "3001-3500" },
-    { value: "3501-4000 msnm", label: "3501-4000" },
-    { value: "> 4001 msnm", label: "> 4001" },
-  ];
-  const longevity = [
-    { value: "Anual", label: "Anual" },
-    { value: "Perenne", label: "Perenne" },
-    { value: "Baja", label: "Baja" },
-    { value: "Media", label: "Media" },
-    { value: "Alta", label: "Alta" },
-    { value: "No determinado", label: "No determinado" },
-  ];
-  const growth_habit = [
-    { value: "Arborea", label: "Arborea" },
-    { value: "Arbustiva", label: "Arbustiva" },
-    { value: "Palmera", label: "Palmera" },
-    { value: "Cactacea", label: "Cactácea" },
-  ];
-  const originValues = [
-    { value: "Nativa", label: "Nativo" },
-    { value: "Endemico", label: "Endemico" },
-  ];
-  const useCategoryValues = [
-    { value: "Alimenticio", label: "Alimenticio" },
-    { value: "Aditivo de los alimentos", label: "Aditivo de los alimentos" },
-    {
-      value: "Alimento de animales vertebrados",
-      label: "Alimento de animales vertebrados",
-    },
-    {
-      value: "Alimento de animales invertebrados",
-      label: "Alimento de animales invertebrados",
-    },
-    { value: "Apícola", label: "Apícola" },
-    { value: "Combustibles", label: "Combustibles" },
-    { value: "Materiales", label: "Materiales" },
-    { value: "Social", label: "Social" },
-    { value: "Tóxico", label: "Tóxico" },
-    { value: "Medicinal", label: "Medicinal" },
-    { value: "Medioambiental", label: "Medioambiental" },
-  ];
-  const publicUseValues = [
-    { value: "No determinado", label: "No determinado" },
-    { value: "Andenes vía de servicio", label: "Andenes vía de servicio" },
-    { value: "Cerros", label: "Cerros" },
-    { value: "Glorietas", label: "Glorietas" },
-    { value: "Orejas de puente", label: "Orejas de puente" },
-    { value: "Parques", label: "Parques" },
-    { value: "Plazas", label: "Plazas/Plazoletas" },
-    { value: "Retiros de quebrada", label: "Retiros de quebrada" },
-    { value: "Separador de autopistas", label: "Separador de autopistas" },
-    { value: "Edificios institucionales", label: "Edificios institucionales" },
-    { value: "Antejardines", label: "Antejardines" },
-    {
-      value: "Separador de arterias principales",
-      label: "Separador de arterias principales",
-    },
-    { value: "Vías peatonales", label: "Vías peatonales" },
-    { value: "Separadores", label: "Separadores" },
-  ];
-  const crownShapeValues = [
-    { value: "Aparasolada", label: "Aparasolada" },
-    { value: "Columnar", label: "Columnar" },
-    { value: "Estratificada", label: "Estratificada" },
-    { value: "Globosa", label: "Globosa" },
-    { value: "Irregular", label: "Irregular" },
-    { value: "Oval", label: "Oval" },
-    { value: "Péndula", label: "Péndula" },
-    { value: "Piramidal", label: "Piramidal" },
-    { value: "Semiglobosa", label: "Semiglobosa" },
-    { value: "No determinada", label: "No determinada" },
-  ];
-  const humidityValues = [
-    { value: "Seca", label: "Seca" },
-    { value: "Humeda", label: "Humeda" },
-    { value: "Muy humeda", label: "Muy humeda" },
-    { value: "No determinada", label: "No determinada" },
-  ];
-  const dispersalValues = [
-    { value: "Anemocoria", label: "Anemocoria (viento)" },
-    { value: "Hidrocoria", label: "Hidrocoria (agua)" },
-    { value: "Baricoria", label: "Baricoria (gravedad)" },
-    { value: "Zoocoria", label: "Zoocoria (animales)" },
-    { value: "Aves", label: "Aves" },
-    { value: "Mamíferos", label: "Mamíferos" },
-    { value: "Insectos", label: "Insectos" },
-    { value: "Aves semilleras", label: "Aves semilleras" },
-    { value: "Aves frugívoras", label: "Aves frugívoras" },
-    { value: "No determinado", label: "No determinado" },
-  ];
-
+  
   return (
     <>
       <Dropdown label="Filtro" arrowIcon={false}>
@@ -238,7 +76,7 @@ const FilterButton = () => {
                   placeholder="No determinado"
                   isClearable={false}
                   isSearchable={false}
-                  options={growth_habit}
+                  options={speciesEnums.growth_habit}
                   onChange={(value: any) => console.log(value)}
                 />
               </div>
@@ -252,7 +90,7 @@ const FilterButton = () => {
                 <label className="text-xs">Rango altitudinal</label>
                 <Select
                   id="altitudinal_range"
-                  options={altitudeRange}
+                  options={speciesEnums.altitudeRange}
                   className="text-xs"
                   placeholder="No determinado"
                   isClearable={false}
@@ -264,7 +102,7 @@ const FilterButton = () => {
                 <label className="text-xs">Origen</label>
                 <Select
                   id="origin"
-                  options={originValues}
+                  options={speciesEnums.originValues}
                   className="text-xs"
                   placeholder="Nativo"
                   isClearable={false}
@@ -276,7 +114,7 @@ const FilterButton = () => {
                 <label className="text-xs">Conservación</label>
                 <Select
                   id="conservation_status"
-                  options={conservationStatus}
+                  options={speciesEnums.conservationStatus}
                   className="text-xs"
                   placeholder="NE"
                   isClearable={false}
@@ -288,7 +126,7 @@ const FilterButton = () => {
                 <label className="text-xs">Atraccion a fauna</label>
                 <Select
                   id="fauna_attraction"
-                  options={priorityLevel}
+                  options={speciesEnums.priorityLevel}
                   className="text-xs"
                   placeholder="No determinado"
                   isClearable={false}
@@ -318,7 +156,7 @@ const FilterButton = () => {
                   id="useCategory"
                   classNamePrefix="select"
                   className="basic-multi-select text-xs"
-                  options={useCategoryValues}
+                  options={speciesEnums.useCategoryValues}
                   onChange={(value) => {
                     if (value !== null) {
                       let data: any = [];
@@ -339,7 +177,7 @@ const FilterButton = () => {
                   id="publicUse"
                   classNamePrefix="select"
                   className="basic-multi-select text-xs"
-                  options={publicUseValues}
+                  options={speciesEnums.publicUseValues}
                   onChange={(value) => {
                     if (value !== null) {
                       let data: any = [];
@@ -384,7 +222,7 @@ const FilterButton = () => {
                 <label className="text-xs">Longevidad</label>
                 <Select
                   id="longevity"
-                  options={longevity}
+                  options={speciesEnums.longevity}
                   className="text-xs"
                   placeholder="No determinado"
                   isClearable={false}
@@ -396,7 +234,7 @@ const FilterButton = () => {
                 <label className="text-xs">Tasa Crecimiento</label>
                 <Select
                   id="growth_rate"
-                  options={growthRate}
+                  options={speciesEnums.growthRate}
                   className="text-xs"
                   placeholder="No determinado"
                   isClearable={false}
@@ -408,7 +246,7 @@ const FilterButton = () => {
                 <label className="text-xs">Requerimientos de luz</label>
                 <Select
                   id="light_requirement"
-                  options={lightRequirement}
+                  options={speciesEnums.lightRequirement}
                   className="text-xs"
                   placeholder="No determinado"
                   isClearable={false}
@@ -420,7 +258,7 @@ const FilterButton = () => {
                 <label className="text-xs">Forma de copa</label>
                 <Select
                   id="crown_shape"
-                  options={crownShapeValues}
+                  options={speciesEnums.crownShapeValues}
                   className="text-xs"
                   placeholder="No determinado"
                   isClearable={false}
@@ -432,7 +270,7 @@ const FilterButton = () => {
                 <label className="text-xs">Densidad de follaje</label>
                 <Select
                   id="foliage_density"
-                  options={priorityLevel}
+                  options={speciesEnums.priorityLevel}
                   className="text-xs"
                   placeholder="No determinado"
                   isClearable={false}
@@ -444,7 +282,7 @@ const FilterButton = () => {
                 <label className="text-xs">Humedad</label>
                 <Select
                   id="humidity_zone"
-                  options={humidityValues}
+                  options={speciesEnums.humidityValues}
                   className="text-xs"
                   placeholder="No determinado"
                   isClearable={false}
@@ -462,7 +300,7 @@ const FilterButton = () => {
                     <label className="text-xs">Tipo de Fruta</label>
                     <Select 
                         id="fruit_type"
-                        options={fruitType}
+                        options={speciesEnums.fruitType}
                         className="text-xs"
                         placeholder="No determinado"
                         isClearable={false}
@@ -474,7 +312,7 @@ const FilterButton = () => {
                     <label className="text-xs">Sistema de Dispersion</label>
                     <Select 
                         id="dispersal"
-                        options={dispersalValues}
+                        options={speciesEnums.dispersalValues}
                         className="text-xs"
                         placeholder="No determinado"
                         isClearable={false}
@@ -495,7 +333,7 @@ const FilterButton = () => {
                     <label className="text-xs">Tipo de Raiz</label>
                     <Select 
                         id="rooting_type"
-                        options={rootingTypes}
+                        options={speciesEnums.rootingTypes}
                         className="text-xs"
                         placeholder="No determinado"
                         isClearable={false}
@@ -516,7 +354,7 @@ const FilterButton = () => {
                     <label className="text-xs">Tiempo de florecimiento</label>
                     <Select 
                         id="flowering_season"
-                        options={floweringSeason}
+                        options={speciesEnums.floweringSeason}
                         className="text-xs"
                         placeholder="No determinado"
                         isClearable={false}
@@ -537,7 +375,7 @@ const FilterButton = () => {
                     <label className="text-xs">Arreglo de flor</label>
                     <Select 
                         id="flower_arrangement"
-                        options={flowerArrangement}
+                        options={speciesEnums.flowerArrangement}
                         className="text-xs"
                         placeholder="No determinado"
                         isClearable={false}
